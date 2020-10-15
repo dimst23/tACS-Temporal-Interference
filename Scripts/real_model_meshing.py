1f27bb6cd48bba1997d4626b200eeaf37356386f
dimst23 <dimitris.apple@gmail.com>
2020-10-15 09:38:12 +0000

    Added all the surfaces of the model

--- Scripts/real_model_meshing.py
+++ Scripts/real_model_meshing.py
@@ -4,38 +4,6 @@ import numpy as np
 import Meshing.mesh_operations as mesh_ops
 import Meshing.electrode_operations as electrode_operations
 
-def electrode_position_sphere(radius, theta, phi=0):
-	return np.array([radius*np.cos(np.deg2rad(phi))*np.cos(np.deg2rad(theta)), radius*np.cos(np.deg2rad(phi))*np.sin(np.deg2rad(theta)), radius*np.sin(np.deg2rad(phi))])
-
-def orient_electrode(mesh, init_point, delta_point):
-	dist = pymesh.signed_distance_to_mesh(mesh, init_point)
-	face = dist[1][0]
-	#
-	# Create a vector with the direction of the face
-	p_1 = mesh.vertices[mesh.faces[face][0]]
-	p_2 = mesh.vertices[mesh.faces[face][1]]
-	dir_vector = p_1 - p_2
-	dir_vector = dir_vector/np.linalg.norm(dir_vector)
-	
-	normal = np.cross(delta_point, dir_vector)
-	return normal/np.linalg.norm(normal)
-
-def add_electrode(surface_mesh, electrode_mesh):
-	# Get the surface outline including the electrode
-	model = pymesh.merge_meshes((surface_mesh, electrode_mesh))
-	outer_hull = pymesh.compute_outer_hull(model)
-	#
-	# Create the surface with the electrode mesh imprinted
-	electrode_tan_mesh = pymesh.boolean(electrode_mesh, surface_mesh, 'difference')
-	outer_diff = pymesh.boolean(outer_hull, electrode_tan_mesh, 'difference')
-	conditioned_surface = pymesh.merge_meshes((outer_diff, electrode_tan_mesh))
-	#
-	# Generate the surface with the electrode on
-	face_id = np.arange(conditioned_surface.num_faces)
-	conditioned_surface = pymesh.remove_duplicated_vertices(conditioned_surface)[0]  # Remove any duplicate vertices
-	#
-	return [pymesh.submesh(conditioned_surface, np.isin(face_id, pymesh.detect_self_intersection(conditioned_surface)[:, 0], invert=True), 0), outer_diff]  # Get rid of the duplicate faces on the tangent surface, without merging the points
-
 MAX_RADIUS = 5
 #base_path = './'
 base_path = '/mnt/d/Thesis/Tests/model/fixed/'
@@ -43,61 +11,134 @@ base_path = '/mnt/d/Thesis/Tests/model/fixed/'
 ##### Import th model files to create the mesh
 print("Importing files") # INFO log
 skin_stl = pymesh.load_mesh(base_path + 'skin_fixed.stl')
+skull_stl = pymesh.load_mesh(base_path + 'skull_fixed.stl')
+csf_stl = pymesh.load_mesh(base_path + 'csf_fixed.stl')
+gm_stl = pymesh.load_mesh(base_path + 'gm_fixed.stl')
+wm_stl = pymesh.load_mesh(base_path + 'wm_fixed.stl')
+ventricles_stl = pymesh.load_mesh(base_path + 'ventricles_fixed.stl')
+cerebellum_stl = pymesh.load_mesh(base_path + 'cerebellum_fixed.stl')
 ##### Import th model files to create the mesh
-
-# Generate the mesh of the model
-print("Merging meshes") # INFO log
-#model = pymesh.merge_meshes((s1_stl, s2_stl, s3_stl))
 
 ##### Electrode placement
 width = 3
 radius = 4
-elements = 100
+elements = 200
 
 skin_stl.enable_connectivity()
 
-p_i_base_vcc = skin_stl.vertices[10421]
+p_i_base_vcc = skin_stl.vertices[1078]
 cr_base_vcc = electrode_operations.orient_electrode(skin_stl, p_i_base_vcc)
 
-p_i_base_gnd = skin_stl.vertices[3187]
+p_i_base_gnd = skin_stl.vertices[2400]
 cr_base_gnd = electrode_operations.orient_electrode(skin_stl, p_i_base_gnd)
 
+p_i_df_vcc = skin_stl.vertices[14246]
+cr_df_vcc = electrode_operations.orient_electrode(skin_stl, p_i_df_vcc)
+
+p_i_df_gnd = skin_stl.vertices[13539]
+cr_df_gnd = electrode_operations.orient_electrode(skin_stl, p_i_df_gnd)
+
 elec_base_vcc = pymesh.generate_cylinder(p_i_base_vcc - (width*cr_base_vcc)/4., p_i_base_vcc + (width*cr_base_vcc)/4., radius, radius, elements)
-
 elec_base_gnd = pymesh.generate_cylinder(p_i_base_gnd - (width*cr_base_gnd)/4., p_i_base_gnd + (width*cr_base_gnd)/4., radius, radius, elements)
+elec_df_vcc = pymesh.generate_cylinder(p_i_df_vcc - (width*cr_df_vcc)/4., p_i_df_vcc + (width*cr_df_vcc)/4., radius, radius, elements)
+elec_df_gnd = pymesh.generate_cylinder(p_i_df_gnd - (width*cr_df_gnd)/4., p_i_df_gnd + (width*cr_df_gnd)/4., radius, radius, elements)
 
-elec_meshes = pymesh.merge_meshes((elec_base_vcc, elec_base_gnd))
-#sub_outer = add_electrode(skin_stl, elec_base_vcc)
-sub_outer = add_electrode(skin_stl, elec_meshes)
-
-# pymesh.save_mesh('br_elec.stl', sub_outer[0])
+elec_meshes = pymesh.merge_meshes((elec_base_vcc, elec_base_gnd, elec_df_vcc, elec_df_gnd))
+sub_outer = electrode_operations.add_electrode(skin_stl, elec_meshes)
 ##### Electrode placement
 
+sub_outer[0] = pymesh.merge_meshes((sub_outer[0], skull_stl, csf_stl, wm_stl, gm_stl, ventricles_stl, cerebellum_stl))
 part_model = pymesh.tetrahedralize(sub_outer[0], MAX_RADIUS)
+
 sp_tet = pymesh.tetrahedralize(sub_outer[1], MAX_RADIUS)
 
-boundary_surfaces = pymesh.form_mesh(sp_tet.vertices, sp_tet.faces)
-#boundary_surfaces = pymesh.separate_mesh(boundary_surfaces)
+boundary_surfaces_elec = pymesh.form_mesh(sp_tet.vertices, sp_tet.faces)
+boundary_surfaces = pymesh.form_mesh(part_model.vertices, part_model.faces)
+boundary_surfaces = pymesh.separate_mesh(boundary_surfaces)
+
+boundary_surfaces = mesh_ops.boundary_order([skin_stl, skull_stl, csf_stl, gm_stl, wm_stl, ventricles_stl, cerebellum_stl], boundary_surfaces)
+
 dom_roi_1 = {
-    'x_min': np.amin(elec_base_vcc.vertices[:, 0]),
-    'x_max': np.amax(elec_base_vcc.vertices[:, 0]),
-    'y_min': np.amin(elec_base_vcc.vertices[:, 1]),
-    'y_max': np.amax(elec_base_vcc.vertices[:, 1]),
-    'z_min': np.amin(elec_base_vcc.vertices[:, 2]),
-    'z_max': np.amax(elec_base_vcc.vertices[:, 2]),
+	'x_min': np.amin(elec_base_vcc.vertices[:, 0]),
+	'x_max': np.amax(elec_base_vcc.vertices[:, 0]),
+	'y_min': np.amin(elec_base_vcc.vertices[:, 1]),
+	'y_max': np.amax(elec_base_vcc.vertices[:, 1]),
+	'z_min': np.amin(elec_base_vcc.vertices[:, 2]),
+	'z_max': np.amax(elec_base_vcc.vertices[:, 2]),
 }
 
 dom_roi_2 = {
-    'x_min': np.amin(elec_base_gnd.vertices[:, 0]),
-    'x_max': np.amax(elec_base_gnd.vertices[:, 0]),
-    'y_min': np.amin(elec_base_gnd.vertices[:, 1]),
-    'y_max': np.amax(elec_base_gnd.vertices[:, 1]),
-    'z_min': np.amin(elec_base_gnd.vertices[:, 2]),
-    'z_max': np.amax(elec_base_gnd.vertices[:, 2]),
+	'x_min': np.amin(elec_base_gnd.vertices[:, 0]),
+	'x_max': np.amax(elec_base_gnd.vertices[:, 0]),
+	'y_min': np.amin(elec_base_gnd.vertices[:, 1]),
+	'y_max': np.amax(elec_base_gnd.vertices[:, 1]),
+	'z_min': np.amin(elec_base_gnd.vertices[:, 2]),
+	'z_max': np.amax(elec_base_gnd.vertices[:, 2]),
 }
 
-mesh_domains_1 = mesh_ops.domain_extract(part_model, boundary_surfaces)
+dom_roi_3 = {
+	'x_min': np.amin(elec_df_vcc.vertices[:, 0]),
+	'x_max': np.amax(elec_df_vcc.vertices[:, 0]),
+	'y_min': np.amin(elec_df_vcc.vertices[:, 1]),
+	'y_max': np.amax(elec_df_vcc.vertices[:, 1]),
+	'z_min': np.amin(elec_df_vcc.vertices[:, 2]),
+	'z_max': np.amax(elec_df_vcc.vertices[:, 2]),
+}
 
+dom_roi_4 = {
+	'x_min': np.amin(elec_df_gnd.vertices[:, 0]),
+	'x_max': np.amax(elec_df_gnd.vertices[:, 0]),
+	'y_min': np.amin(elec_df_gnd.vertices[:, 1]),
+	'y_max': np.amax(elec_df_gnd.vertices[:, 1]),
+	'z_min': np.amin(elec_df_gnd.vertices[:, 2]),
+	'z_max': np.amax(elec_df_gnd.vertices[:, 2]),
+}
+mesh_domains_elec = mesh_ops.domain_extract(part_model, boundary_surfaces_elec)
+
+elecs_l, st_srf = electrode_operations.electrodes_separate(part_model, mesh_domains_elec, [dom_roi_1, dom_roi_2, dom_roi_3, dom_roi_4])
+
+boundary_order = [4, 6, 1, 2, 3, 5]
+extraction_direction = [['in', 6], ['in', 5], ['out', 1], ['out', 2], ['out', 3], ['out', 4]]
+zero_operations = [[True, False], [False, True], [True, False], [True, False], [False, True], [True, False]]
+output_order = {
+	'Cerebellum': 5,
+	'Ventricles': 2,
+	'Skin': 0,
+	'Skull': 1,
+	'CSF': 2,
+	'GM': 3,
+	'WM': 4,
+}
+
+msh_f = mesh_ops.mesh_form([st_srf, boundary_surfaces], extraction_direction, zero_operations, output_order)
+msh_f = mesh_ops.mesh_conditioning(msh_f) # Remove duplicate elements
+
+# Save the mesh is VTK format
+import meshio
+
+em_c = np.empty(part_model.num_voxels)
+em_p = np.empty(part_model.num_vertices)
+
+i = 6
+for key in msh_f.keys():
+	em_c[msh_f[key]['mesh'].get_attribute("ori_voxel_index").astype(np.int32)] = msh_f[key]['id']
+	em_p[np.unique(part_model.voxels[msh_f[key]['mesh'].get_attribute("ori_voxel_index").astype(np.int32)])] = msh_f[key]['id']
+
+for elec in elecs_l:
+	em_c[elec.get_attribute("ori_voxel_index").astype(np.int32)] = i
+	em_p[np.unique(part_model.voxels[elec.get_attribute("ori_voxel_index").astype(np.int32)])] = i
+	i = i + 1
+
+mss = meshio.Mesh(
+	points=part_model.vertices,
+	point_data={"node_groups": em_p},
+	cells={"tetra": part_model.voxels},
+	cell_data={"mat_id": em_c},
+)
+
+mss.write('msh_real.vtk')
+
+'''
 #elec_sep_vcc = electrode_operations.electrode_separate(mesh_domains_1[1], dom_roi_1)
 #elec_sep_gnd = electrode_operations.electrode_separate(elec_sep_vcc[1], dom_roi_2)
 
@@ -133,3 +174,4 @@ mss = meshio.Mesh(
 
 mss.write('msh.vtk')
 #pymesh.save_mesh('sphere_elec.msh', part_model)
+'''
