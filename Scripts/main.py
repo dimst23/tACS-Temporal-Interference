fe1b950fc70a1a3e1af61c22e07db194259c7faa
dimst23 <dimitris.apple@gmail.com>
2020-12-31 17:39:55 +0000

    Included the new classes

--- Scripts/main.py
+++ Scripts/main.py
@@ -1,27 +1,26 @@
 import os
-import Meshing.phm_model_meshing as model_meshing
+import Meshing.MeshOperations as MeshOps
 
-import meshio
 import scipy.io as sio
 
-base_path = '/mnt/c/Users/Dimitris/Downloads/Head_Models_Fixed/'
+base_path = '/home/dimitris/tetgen1.6.0/build'
 # folders = next(os.walk(base_path))[1]
 #folders = ['103414', '105014', '105115', '110411', '111716', '113619', '117122', '163129', '196750']
-folders = ['103414']
+folders = ['105014']
 
 if __name__ == '__main__':
-	for folder in folders:
-		print("############")
-		print("Model " + folder)
-		print("############\n")
-		standard_electrodes = sio.loadmat(os.path.join(base_path, folder, '10-20_elec_' + folder + '.mat'))
-		elec_attributes = {
-			'names': [name[0][0] for name in standard_electrodes['ElectrodeNames']],
-			'coordinates': standard_electrodes['ElectrodePts'],
-			'width': 3,
-			'radius': 4,
-			'elements': 200,
-		}
+    for folder in folders:
+        print("############")
+        print("Model " + folder)
+        print("############\n")
+        standard_electrodes = sio.loadmat(os.path.join(base_path, folder, '10-20_elec_' + folder + '.mat'))
+        elec_attributes = {
+            'names': [name[0][0] for name in standard_electrodes['ElectrodeNames']],
+            'coordinates': standard_electrodes['ElectrodePts'],
+            'width': 3,
+            'radius': 4,
+            'elements': 200,
+        }
 
-		mesh = model_meshing.phm_model_meshing(os.path.join(base_path, folder), '_fixed.stl', elec_attributes)
-		mesh['mesh'].write(os.path.join(base_path, folder, 'mesh_' + folder + '_10-20.vtk'))
+        meshing = MeshOps.MeshOperations()
+        meshing.phm_model_meshing(os.path.join(base_path, folder), '_fixed.stl', 'first_poly_try.poly', elec_attributes)
