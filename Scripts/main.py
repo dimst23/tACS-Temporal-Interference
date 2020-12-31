b087801e5f6da94c33d65efa59d3f1916c45f874
dimst23 <dimitris.apple@gmail.com>
2020-12-31 22:38:56 +0000

    Archived the SfePy problem description files

--- Scripts/main.py
+++ Scripts/main.py
@@ -3,10 +3,11 @@ import Meshing.MeshOperations as MeshOps
 
 import scipy.io as sio
 
-base_path = '/home/dimitris/tetgen1.6.0/build'
+base_path = '/home/dimitris/tetgen1.6.0/models'
 # folders = next(os.walk(base_path))[1]
-#folders = ['103414', '105014', '105115', '110411', '111716', '113619', '117122', '163129', '196750']
-folders = ['105014']
+folders = ['103414', '105014', '105115', '110411', '111716', '113619', '117122', '163129', '196750']
+#folders = ['105115', '110411', '111716', '113619', '117122', '163129', '196750']
+#folders = ['105115']
 
 if __name__ == '__main__':
     for folder in folders:
@@ -17,10 +18,10 @@ if __name__ == '__main__':
         elec_attributes = {
             'names': [name[0][0] for name in standard_electrodes['ElectrodeNames']],
             'coordinates': standard_electrodes['ElectrodePts'],
-            'width': 3,
+            'width': 4,
             'radius': 4,
             'elements': 200,
         }
 
         meshing = MeshOps.MeshOperations()
-        meshing.phm_model_meshing(os.path.join(base_path, folder), '_fixed.stl', 'first_poly_try.poly', elec_attributes)
+        meshing.phm_model_meshing(os.path.join(base_path, folder), '_fixed.stl', os.path.join(base_path, 'meshed', 'vtk','model_' + folder + '.poly'), elec_attributes)
