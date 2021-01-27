216a139874df5109d8d79539652c90e7b6a74f53
dimst23 <dimitris.apple@gmail.com>
2021-01-27 10:23:17 +0000

    Added support for spherical model meshing

--- Scripts/Meshing/ElectrodeOperations.py
+++ Scripts/Meshing/ElectrodeOperations.py
@@ -3,9 +3,9 @@ import numpy as np
 
 class ElectrodeOperations:
     def __init__(self, surface_mesh: pymesh.Mesh, electrode_attributes: dict):
-        self.surface_mesh = surface_mesh
-        self.surface_mesh.enable_connectivity() # Required by some functions below
-        self.electrode_attributes = electrode_attributes
+        self._surface_mesh = surface_mesh
+        self._surface_mesh.enable_connectivity() # Required by some functions below
+        self._electrode_attributes = electrode_attributes
         self.electrode_array = {}
 
     def add_electrodes_on_skin(self):
@@ -13,11 +13,11 @@ class ElectrodeOperations:
             raise AttributeError('The electrodes shall be positioned first before added on the surface. Please call the positioning function first.')
         electrode_mesh = self.get_electrode_single_mesh()
         # Get the surface outline including the electrode
-        model = pymesh.merge_meshes((self.surface_mesh, electrode_mesh))
+        model = pymesh.merge_meshes((self._surface_mesh, electrode_mesh))
         outer_hull = pymesh.compute_outer_hull(model)
 
         # Create the surface with the electrode mesh imprinted
-        electrode_tan_mesh = pymesh.boolean(electrode_mesh, self.surface_mesh, 'difference')
+        electrode_tan_mesh = pymesh.boolean(electrode_mesh, self._surface_mesh, 'difference')
         outer_diff = pymesh.boolean(outer_hull, electrode_tan_mesh, 'difference')
         conditioned_surface = pymesh.merge_meshes((outer_diff, electrode_tan_mesh))
 
@@ -27,18 +27,38 @@ class ElectrodeOperations:
 
         return [pymesh.submesh(conditioned_surface, np.isin(face_id, pymesh.detect_self_intersection(conditioned_surface)[:, 0], invert=True), 0), outer_diff]  # Get rid of the duplicate faces on the tangent surface, without merging the points
 
-    def standard_electrode_positioning(self, width=4, radius=4, elements=150):
-        closest_point = pymesh.distance_to_mesh(self.surface_mesh, self.electrode_attributes['coordinates'])[1] # Get the closest point to the one provided
+    def standard_electrode_positioning(self):
+        width = self._electrode_attributes['width']
+        radius = self._electrode_attributes['radius']
+        elements = self._electrode_attributes['elements']
+
+        closest_point = pymesh.distance_to_mesh(self._surface_mesh, self._electrode_attributes['coordinates'])[1] # Get the closest point to the one provided
 
         i = 0
-        for electrode_name in self.electrode_attributes['names']:
-            p_i = self.surface_mesh.vertices[self.surface_mesh.faces[closest_point[i]]][0] # Get the surface vertex coordinates
+        for electrode_name in self._electrode_attributes['names']:
+            p_i = self._surface_mesh.vertices[self._surface_mesh.faces[closest_point[i]]][0] # Get the surface vertex coordinates
             electrode_orientation = self.__orient_electrode(p_i) # Orient the electrode perpendicular to the surface
 
             # Generate the electrode cylinder and save to the output dictionary
             electrode_cylinder = pymesh.generate_cylinder(p_i - (width * electrode_orientation)/4., p_i + (width * electrode_orientation)/4., radius, radius, elements)
             self.electrode_array[electrode_name] = electrode_cylinder
             i = i + 1
+
+    def sphere_electrode_positioning(self):
+        cyl_radius = self._electrode_attributes['cylinder_radius']
+        #skin_radius = self._electrode_attributes['skin_radius']
+        skin_radius = np.amax(self._surface_mesh.vertices[:, 0])
+        cyl_width = self._electrode_attributes['cylinder_width']
+        elements = self._electrode_attributes['elements']
+
+        for electrode in self._electrode_attributes['electrodes'].items():
+            print(electrode)
+            p_i = self.electrode_position_sphere(skin_radius, electrode[1]['theta'], electrode[1]['phi'])
+            electrode_orientation = self.__orient_electrode_sphere(p_i, np.array([0, 0, -cyl_width]))
+
+            # Generate the electrode cylinder and save to the output dictionary
+            electrode_cylinder = pymesh.generate_cylinder(p_i - (cyl_width * electrode_orientation)/4., p_i + (cyl_width * electrode_orientation)/4., cyl_radius, cyl_radius, elements)
+            self.electrode_array[electrode[0]] = electrode_cylinder
 
     def get_electrode_array(self):
         if not self.electrode_array:
@@ -59,11 +79,11 @@ class ElectrodeOperations:
         Returns:
             [type]: [description]
         """
-        point_id = np.where(np.sum(self.surface_mesh.vertices == init_point, axis=1))[0][0] # Unique point assumed
-        face = self.surface_mesh.get_vertex_adjacent_faces(point_id)[0]
+        point_id = np.where(np.sum(self._surface_mesh.vertices == init_point, axis=1))[0][0] # Unique point assumed
+        face = self._surface_mesh.get_vertex_adjacent_faces(point_id)[0]
 
         points = []
-        for point in self.surface_mesh.vertices[self.surface_mesh.faces[face]]:
+        for point in self._surface_mesh.vertices[self._surface_mesh.faces[face]]:
             if np.sum(point != init_point):
                 points.append(point)
         p_1 = points[0] - init_point
@@ -71,4 +91,21 @@ class ElectrodeOperations:
 
         normal = np.cross(p_1, p_2)
         return normal/np.linalg.norm(normal)
+
+    def __orient_electrode_sphere(self, init_point, delta_point):
+        dist = pymesh.signed_distance_to_mesh(self._surface_mesh, init_point)
+        face = dist[1][0]
+
+        # Create a vector with the direction of the face
+        p_1 = self._surface_mesh.vertices[self._surface_mesh.faces[face][0]]
+        p_2 = self._surface_mesh.vertices[self._surface_mesh.faces[face][1]]
+        dir_vector = p_1 - p_2
+        dir_vector = dir_vector/np.linalg.norm(dir_vector)
+
+        normal = np.cross(delta_point, dir_vector)
+        return normal/np.linalg.norm(normal)
+
+    @staticmethod
+    def electrode_position_sphere(radius, theta, phi=0):
+        return np.array([radius*np.cos(np.deg2rad(phi))*np.cos(np.deg2rad(theta)), radius*np.cos(np.deg2rad(phi))*np.sin(np.deg2rad(theta)), radius*np.sin(np.deg2rad(phi))])
 
--- Scripts/Meshing/MeshOperations.py
+++ Scripts/Meshing/MeshOperations.py
@@ -1,45 +1,59 @@
 import os
-import meshio
 import pymesh
 import numpy as np
 
 import Meshing.ElectrodeOperations as ElecOps
 import FileOps.FileOperations as FileOps
 
-class MeshOperations(ElecOps.ElectrodeOperations):
-    def __init__(self):
+class MeshOperations(ElecOps.ElectrodeOperations, FileOps.FileOperations):
+    def __init__(self, surface_mesh: pymesh.Mesh, electrode_attributes: dict):
         self.merged_meshes = None
-        print()
+        self.skin_with_electrodes = None
+        self.electrode_mesh = None
+        self.surface_meshes = []
+        ElecOps.ElectrodeOperations.__init__(self, surface_mesh, electrode_attributes)
 
-    def phm_model_meshing(self, base_path: str, suffix_name: str, mesh_filename: str, electrode_attributes: dict):
-        ##### Import th model files to create the mesh
-        print("Importing files") # INFO log
-        skin_stl = pymesh.load_mesh(os.path.join(base_path, 'skin' + suffix_name))
-        skull_stl = pymesh.load_mesh(os.path.join(base_path, 'skull' + suffix_name))
-        csf_stl = pymesh.load_mesh(os.path.join(base_path, 'csf' + suffix_name))
-        gm_stl = pymesh.load_mesh(os.path.join(base_path, 'gm' + suffix_name))
-        wm_stl = pymesh.load_mesh(os.path.join(base_path, 'wm' + suffix_name))
-        ventricles_stl = pymesh.load_mesh(os.path.join(base_path, 'ventricles' + suffix_name))
-        cerebellum_stl = pymesh.load_mesh(os.path.join(base_path, 'cerebellum' + suffix_name))
-        ##### Import th model files to create the mesh
+    def phm_model_meshing(self, mesh_filename: str):
+        if not self.surface_meshes:
+            raise AttributeError('Meshes must be loaded first. Please load the meshes.')
+        self.electrode_meshing()
 
-        ##### Electrode placement
+        self.merged_meshes = pymesh.merge_meshes((self.skin_with_electrodes, *self.surface_meshes[1:]))
+        regions = self.region_points(self.surface_meshes, 0.1, electrode_mesh=self.electrode_mesh)
+
+        self.poly_write(mesh_filename, self.merged_meshes.vertices, self.merged_meshes.faces, regions)
+
+    def sphere_model_meshing(self, mesh_filename: str):
+        if not self.surface_meshes:
+            raise AttributeError('Meshes must be loaded first. Please load the meshes.')
+        self.electrode_meshing(True)
+
+        self.merged_meshes = pymesh.merge_meshes((self.skin_with_electrodes, *self.surface_meshes[1:]))
+        regions = self.region_points(self.surface_meshes, 0.1, electrode_mesh=self.electrode_mesh)
+
+        self.poly_write(mesh_filename, self.merged_meshes.vertices, self.merged_meshes.faces, regions)
+
+    def load_surface_meshes(self, base_path: str, file_names: list):
+        # TODO: Order matters
+        for file_name in file_names:
+            self.surface_meshes.append(pymesh.load_mesh(os.path.join(base_path, file_name)))
+
+    def electrode_meshing(self, sphere=False):
         print("Placing Electrodes") # INFO log
-        electrodes_object = ElecOps.ElectrodeOperations(skin_stl, electrode_attributes)
-        electrodes_object.standard_electrode_positioning(width=electrode_attributes['width'], radius=electrode_attributes['radius'], elements=electrode_attributes['elements'])
-        skin_with_electrodes = electrodes_object.add_electrodes_on_skin()[0]
+        if sphere:
+            self.sphere_electrode_positioning()
+        else:
+            self.standard_electrode_positioning()
+        self.skin_with_electrodes = self.add_electrodes_on_skin()[0]
 
-        electrodes_single_mesh = electrodes_object.get_electrode_single_mesh()
-        electrodes = pymesh.boolean(electrodes_single_mesh, skin_stl, 'difference')
-        pymesh.map_face_attribute(electrodes_single_mesh, electrodes, 'face_sources')
-        ##### Electrode placement
+        electrodes_single_mesh = self.get_electrode_single_mesh()
+        self.electrode_mesh = pymesh.boolean(electrodes_single_mesh, self._surface_mesh, 'difference')
+        pymesh.map_face_attribute(electrodes_single_mesh, self.electrode_mesh, 'face_sources')
 
-        self.merged_meshes = pymesh.merge_meshes((skin_with_electrodes, skull_stl, csf_stl, gm_stl, wm_stl, cerebellum_stl, ventricles_stl))
-        regions = self.region_points([skin_stl, skull_stl, csf_stl, gm_stl, wm_stl, cerebellum_stl, ventricles_stl], electrodes, 0.1)
+        return self.electrode_mesh
 
-        FileOps.FileOperations.poly_write(mesh_filename, self.merged_meshes.vertices, self.merged_meshes.faces, self.merged_meshes.get_attribute('face_sources'), regions)
-
-    def region_points(self, boundary_surfaces: list, electrode_mesh, shift: float, max_volume=30):
+    @staticmethod
+    def region_points(boundary_surfaces: list, shift: float, electrode_mesh=None, max_volume=30):
         points = {}
         i = 1
         for surface in boundary_surfaces:
@@ -51,18 +65,19 @@ class MeshOperations(ElecOps.ElectrodeOperations):
             }
             i = i + 1
 
-        i = 10
-        electrode_regions = electrode_mesh.get_attribute('face_sources')
-        electrode_mesh.add_attribute('vertex_valance')
-        vertex_valance = electrode_mesh.get_attribute('vertex_valance')
-        for region in np.unique(electrode_regions):
-            faces = electrode_mesh.faces[np.where(electrode_regions == region)[0]]
-            vertices = electrode_mesh.vertices[np.unique(faces)]
-            max_valance_point = np.where(vertex_valance[np.unique(faces)] == np.amax(vertex_valance[np.unique(faces)]))[0][0]
-            points[str(i)] = {
-                'coordinates': np.round(vertices[max_valance_point] - np.multiply(vertices[max_valance_point]/np.linalg.norm(vertices[max_valance_point]), shift), 6),
-                'max_volume': max_volume,
-            }
-            i = i + 1
+        if electrode_mesh is not None:
+            i = 10
+            electrode_regions = electrode_mesh.get_attribute('face_sources')
+            electrode_mesh.add_attribute('vertex_valance')
+            vertex_valance = electrode_mesh.get_attribute('vertex_valance')
+            for region in np.unique(electrode_regions):
+                faces = electrode_mesh.faces[np.where(electrode_regions == region)[0]]
+                vertices = electrode_mesh.vertices[np.unique(faces)]
+                max_valance_point = np.where(vertex_valance[np.unique(faces)] == np.amax(vertex_valance[np.unique(faces)]))[0][0]
+                points[str(i)] = {
+                    'coordinates': np.round(vertices[max_valance_point] - np.multiply(vertices[max_valance_point]/np.linalg.norm(vertices[max_valance_point]), shift), 6),
+                    'max_volume': max_volume,
+                }
+                i = i + 1
 
         return points
